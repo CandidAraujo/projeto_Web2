69b52f060b332012302e93dfbeba7dbe9a10c344
candidAraujo <ccandida572@gmail.com>
2021-01-14 11:08:53 -0300

    Inserindo scripts das tabelas

---
+++ projetoWeb2/database/migrations/2021_01_14_120032_create_atletas_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAtletasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('atletas', function (Blueprint $table) {
+            $table->id();
+            $table->integer('treinador_id');
+            $table->string('nome');
+            $table->string('email');
+            $table->string('telefone');
+            $table->string('modalidade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('atletas');
+    }
+}
